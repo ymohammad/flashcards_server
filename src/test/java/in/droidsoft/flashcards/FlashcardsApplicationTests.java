3338f61c5f72ea121d0b25c02969f38d3e664b84
Mohammad Sufyan Al Yousufi <mdsufyan2005@gmail.com>
2021-04-25 14:30:01 +0530

    Initial Commit

---
+++ src/test/java/in/droidsoft/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.droidsoft.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
